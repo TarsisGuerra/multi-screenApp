66e53f6cfd18c5d8afbc10947ba7f788e6cdaff4
Társis Guerra <tarsisguerra@hotmail.com>
2025-11-11 09:33:08 -0300

    Accessing Data by Constructor

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../models/category.dart';
+
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({super.key});
+  final Category category;
+
+  const CategoriesMealsScreen(this.category, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Receitas')),
-      body: Center(child: Text('Receitas por Categoria')),
+      appBar: AppBar(title: Text(category.title)),
+      body: Center(child: Text('Receitas por Categoria ${category.id}')),
     );
   }
 }
