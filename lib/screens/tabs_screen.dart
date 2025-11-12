245cb3ba7a0b0772f08f8674739d7a77dc0061e6
Társis Guerra <tarsisguerra@hotmail.com>
2025-11-11 23:01:43 -0300

    Bottom NavigationBar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,38 +2,48 @@ import 'package:flutter/material.dart';
 import 'categories_screens.dart';
 import 'favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
 
   @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+  final List<Map<String, Object>> _screen = [
+    {'title': 'Lista de Categorias', 'screen': CategoriesScreen()},
+    {'title': 'Meus Favoritos', 'screen': FavoriteScreen()},
+  ];
+
+  _selectedScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).primaryColor,
-          title: Center(
-            child: Text(
-              'Vamos cozinhar?',
-              style: TextStyle(color: Colors.white),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(_screen[_selectedScreenIndex]['title'] as String),
+      ),
+      body: _screen[_selectedScreenIndex]['screen'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).highlightColor,
+        currentIndex: _selectedScreenIndex,
+        onTap: _selectedScreen,
+
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categorias',
           ),
-          bottom: TabBar(
-            unselectedLabelColor: Colors.white30,
-            labelColor: Colors.white,
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category, color: Colors.white),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.star, color: Colors.white),
-                text: 'Favoritos',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(children: [CategoriesScreen(), FavoriteScreen()]),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favoritos'),
+        ],
       ),
     );
   }
